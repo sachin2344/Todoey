046b180eef8a60ca9cfbaa8c5cb72c58170f85d9
sachin sharma <sachinsharma2344@gmail.com>
2019-05-20 17:35:04 +0530

    convert to MVC design pattern

---
+++ Todoey/DataModal/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by sachin sharma on 20/05/19.
+//  Copyright © 2019 sachin sharma. All rights reserved.
+//
+
+import Foundation
+class Item{
+    var title: String = ""
+    var done: Bool = false
+    
+}
